ab25582cac812b4122ba4890161bb80ae7f4a88c
Suan Sen Yeo <suansen88@gmail.com>
2021-11-29 18:14:18 +0800

    Center index partial complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying users and centers"
+User.destroy_all
+Center.destroy_all
+
+puts "initialize seed"
+
+10.times do
+  puts "creating user"
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '1234abcd'
+  )
+  puts user.first_name
+end
+
+5.times do
+  puts "creating dive center for first 2 users"
+  center = Center.create!(
+    name: Faker::Company.name,
+    description: Faker::Company.catch_phrase,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Company.australian_business_number,
+    email: Faker::Internet.email ,
+    location: Faker::Address.city,
+    user: User.first
+  )
+end
