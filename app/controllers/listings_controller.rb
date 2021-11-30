323a404f6a221d7e783a44873f9683c983167356
jensen yuen <jensenyuen@outlook.com>
2021-11-30 15:17:52 +0800

    action added to listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,16 @@
 class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show, :edit, :destroy]
   def index_trips
     @trips = Listing.where(category: "Diving")
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  private
+
+  def find_listing
+    @listing = Listing.find(params[:id])
   end
 end
