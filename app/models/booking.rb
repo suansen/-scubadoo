edfe883eb19f24b0d185b0d275f748e639c58384
Sufian Suni <sufiansuni@gmail.com>
2021-11-30 11:14:09 +0800

    association fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,5 @@ class Booking < ApplicationRecord
   validates :no_of_divers, presence: true, numericality: { only_integer: true, greater_than: 0.00 }
   validates :status, presence: true
   validates :costs, presence:true
+  validates :listing, presence:true
 end
--- db/migrate/20211130025508_create_bookings.rb
+++ db/migrate/20211130025508_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :listing
+      t.references :listing, null: false, foreign_key: true
       t.integer :no_of_divers
       t.string :status
       t.integer :costs
