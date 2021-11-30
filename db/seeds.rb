71a260bb6d30227effe74d2ff9650f9e1ed54727
jensen yuen <jensenyuen@outlook.com>
2021-11-30 11:21:49 +0800

    Added Listing to seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Destroying users and centers..'
+Listing.destroy_all
 Center.destroy_all
 User.destroy_all
 puts 'Users and centers destroyed!'
