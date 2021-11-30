a51cdd677398de99ea61a2611c2073d148ed5c5d
Sufian Suni <sufiansuni@gmail.com>
2021-11-30 12:37:56 +0800

    added listings controller and initial trip page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :centers
+  resources :listings
+
+  get "/trips", to: "listings#index_trips"
+  get "/courses", to: "listings#index_courses"
 end
