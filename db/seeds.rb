26b3e00df1d3ec4a3a154399ad3a29123c4b0d7a
jensen yuen <jensenyuen@outlook.com>
2021-11-30 11:21:08 +0800

    seed file to include listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Destroying users and centers"
-User.destroy_all
+puts 'Destroying users and centers..'
 Center.destroy_all
+User.destroy_all
+puts 'Users and centers destroyed!'
 
-puts "initialize seed"
+puts 'initialize seed..'
 
+puts 'creating user'
 10.times do
-  puts "creating user"
-  user = User.create!(
+  User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: '1234abcd'
   )
-  puts user.first_name
 end
 
+puts 'creating dive center for first 2 users'
 5.times do
-  puts "creating dive center for first 2 users"
-  center = Center.create!(
+  Center.create!(
     name: Faker::Company.name,
     description: Faker::Company.catch_phrase,
     address: Faker::Address.street_address,
     phone_number: Faker::Company.australian_business_number,
-    email: Faker::Internet.email ,
+    email: Faker::Internet.email,
     location: Faker::Address.city,
     user: User.first
   )
 end
+
+CATEGORY = ["Diving", "course"]
+10.times do
+  puts 'creating listings for the first dive center'
+  Listing.create!(
+    category: CATEGORY.sample,
+    name: CATEGORY.sample,
+    description: Faker::Lorem.paragraphs,
+    price: rand(100..250),
+    date: Faker::Date.forward(days: 1),
+    start_time: Faker::Time.forward(days: 1, period: :morning),
+    duration: rand(1..72),
+    dive_count: rand(1..4),
+    max_divers: rand(2..8),
+    center: Center.first
+  )
+end
+
+puts 'seeding completed!'
