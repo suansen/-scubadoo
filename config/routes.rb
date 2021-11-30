929f8b25228e6ace81a0971313485b8b8c60d699
jensen yuen <jensenyuen@outlook.com>
2021-11-30 15:18:03 +0800

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :centers
-  resources :listings
+  resources :listings, only: [:show, :edit, :update, :destroy]
+
+  resources :listings do
+    resources :booking, only: [:new, :create]
+  end
 
   get "/trips", to: "listings#index_trips"
   get "/courses", to: "listings#index_courses"
