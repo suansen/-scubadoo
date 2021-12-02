c5d538c20f85c065dbc92f0893e31eeac5745143
jensen yuen <jensenyuen@outlook.com>
2021-12-02 17:03:32 +0800

    Added new gem for exporting to pdf

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   get "/trips", to: "listings#index_trips"
   get "/courses", to: "listings#index_courses"
   patch "/bookings/:id/cancel", to: "bookings#cancel", as: "cancel_booking"
+  get "/bookings/:id/export.pdf", to: "bookings#export", as: "export_booking"
 end
