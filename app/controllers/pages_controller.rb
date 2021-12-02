58abe339b232438ad1c32b3a4175d4314062101c
Suan Sen Yeo <suansen88@gmail.com>
2021-12-03 02:17:37 +0800

    changed top destinations to dynamic from dive center locations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @locations = []
+    center_array = Center.all.uniq { |center| center.location.strip }
+    center_array.each do |center|
+      @locations << center.location.strip
+    end
   end
 end
