babaa550155ae487bda5eaa3167a597d1877bd2d
eztree <huienlim@gmail.com>
2022-01-03 19:46:43 +0800

    Many frontend fixes on the homepage~

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,3 +33,14 @@ document.addEventListener("turbolinks:load", () => {
   // initAutocomplete();
   checkoutValidation();
 });
+
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
